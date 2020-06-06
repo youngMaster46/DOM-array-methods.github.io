68a812725fd3370c1ddf5aa9516913eceadb4667
Maxim Kaplin <kaplin95@inbox.ru>
2020-06-06 13:04:41 +0300

    add async function for fetch data from server

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const doubleBtn = document.getElementById('double')
 const showMillionairesBtn = document.getElementById('show-millionaires')
 const sortBtn = document.getElementById ('sort')
 const calculateWealthBtn = document.getElementById('calculate-wealth')
+
+let data = []
+
+// Fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api')
+    const data = await res.json();
+
+    const user = data.results[0]
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+
+   addData(newUser)
+}
+function addData(obj) {
+    data.push(obj)
+}
+getRandomUser()
+getRandomUser()
+getRandomUser()
