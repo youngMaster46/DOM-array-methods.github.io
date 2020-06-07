61a13532da12538fab0d0d3866ec140c0a7a69fd
Maxim Kaplin <kaplin95@inbox.ru>
2020-06-07 12:42:39 +0300

    add calculating all summ of wealth

--- script.js
+++ script.js
@@ -63,6 +63,17 @@ function showMillionaires() {
     updateDOM()
 }
 
+// Calculate full wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, current) => {
+        return acc + current.money
+    },0)
+
+    const wealthEl = document.createElement('div')
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong> </h3>`
+    main.appendChild(wealthEl)
+}
+
 // Format number as money
 function formatMoney(number) {
     return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
@@ -73,7 +84,7 @@ addUserBtn.addEventListener('click', getRandomUser)
 doubleBtn.addEventListener('click', doubleMoney)
 sortBtn.addEventListener('click', sortByRichest)
 showMillionairesBtn.addEventListener('click', showMillionaires)
-
+calculateWealthBtn.addEventListener('click', calculateWealth)
 getRandomUser()
 getRandomUser()
 getRandomUser()
